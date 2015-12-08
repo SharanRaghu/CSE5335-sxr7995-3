20d7116484dd05fc6f9ae23f0e1f7ff6f39e3de0
Sharan <sharan.raghu@mavs.uta.edu>
2015-12-08 16:59:38 -0600

    Changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,3 @@
-
-#class WelcomeController < ApplicationController
 require 'json'
 require 'open-uri'
 require 'rubygems'
@@ -14,25 +12,19 @@ var = 0
 
 quotes="'"
 
-#db = Mongo::MongoClient.new("localhost", "27017").db("Movie")
 db = Mongo::Client.new([ 'ds041924.mongolab.com:41924' ], :database => 'heroku_vc7qfh9v', :user => 'sharan', :password => 'sharan')
 puts '-------------------------------------------------------------------------------------------------------------------------'
-
-#@coll = db.collection("movies")
-#@coll.remove()
 
 while i < 1 do
 
     data = JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=editors&api_key=4d7847876fa96f67f881aaf1b73e0e30&format=json").read)
       
-      # iterate through the Array of returned artists and print their names                                                                                 
+      	# iterate through the Array of returned artists and print their names                                                                                 
         data["similarartists"]["artist"].each do |artist|
 	      #puts "#{testvar}"
-    	  #insertintable = @coll.insert({ID: var.to_s, Title: artist["name"]})
-      	  insertintable = db[:heroku_vc7qfh9v].insert_one({ID: var.to_s, Title: artist["name"]})
+    	  insertintable = db[:heroku_vc7qfh9v].insert_one({ID: var.to_s, Title: artist["name"]})
          
-            #0  puts artist["name"];
-    	  var = var + 1;
+          var = var + 1;
     	end
 
     i = i + 1;
@@ -43,11 +35,9 @@ puts '--------------------------------------------------------------------------
 #end
 
 #helper_method :datasend
-def datasend
-	db = Mongo::Client.new([ 'ds041924.mongolab.com:41924' ], :database => 'heroku_vc7qfh9v', :user => 'sharan', :password => 'sharan')
- #   @coll = db.collection("movies")
-
-	k=params[:Id]
-    rs2 = @coll.find({:ID => k}).to_json
-    render :nothing => true, :json => rs2
-end
+#def datasend
+#	db = Mongo::Client.new([ 'ds041924.mongolab.com:41924' ], :database => 'heroku_vc7qfh9v', :user => 'sharan', :password => 'sharan')
+#	k=params[:Id]
+#    rs2 = @coll.find({:ID => k}).to_json
+#    render :nothing => true, :json => rs2
+#end
