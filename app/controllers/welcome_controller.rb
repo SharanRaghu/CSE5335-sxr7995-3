de68ef2efcb571212cf22e152cd0a2b5639dd08f
Sharan <sharan.raghu@mavs.uta.edu>
2015-12-08 17:30:21 -0600

    Changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,39 +5,13 @@ require 'mongo'
 
 include Mongo
 
+	def index
+	end
 
-#def index
-i = 0
-var = 0
-
-quotes="'"
-
-db = Mongo::Client.new([ 'ds041924.mongolab.com:41924' ], :database => 'heroku_vc7qfh9v', :user => 'sharan', :password => 'sharan')
-puts '-------------------------------------------------------------------------------------------------------------------------'
-
-while i < 1 do
-
-    data = JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=editors&api_key=4d7847876fa96f67f881aaf1b73e0e30&format=json").read)
-      
-      	# iterate through the Array of returned artists and print their names                                                                                 
-        data["similarartists"]["artist"].each do |artist|
-	      #puts "#{testvar}"
-    	  insertintable = db[:heroku_vc7qfh9v].insert_one({ID: var.to_s, Title: artist["name"]})
-         
-          var = var + 1;
-    	end
-
-    i = i + 1;
-
-end
-
-puts '-------------------------------------------------------------------------------------------------------------------------'
-#end
-
-#helper_method :datasend
-def datasend
+	def datasend
 	db = Mongo::Client.new([ 'ds041924.mongolab.com:41924' ], :database => 'heroku_vc7qfh9v', :user => 'sharan', :password => 'sharan')
+	
 	k=params[:Id]
 	rs2 = db[:heroku_vc7qfh9v].find({:ID => k}).to_json
 	render :json => rs2
-end
+	end
